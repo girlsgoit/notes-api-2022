498739169db0e268f7a7e84210143b9ad7d04628
Marc Graciov <marc.graciov@pm.me>
2022-07-25 16:05:43 +0300

    added readme and docs

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('/login', obtain_auth_token, name='login'),
+    path('login', obtain_auth_token, name='login'),
 ]
