b20176baf0aaa0989220a7fed88afa9973ff48c9
Gojinevschi04 <annagojinevschi@gmail.com>
2022-07-27 17:56:22 +0300

    Add NoteSerilizer

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,5 +1,6 @@
+from dataclasses import fields
 from rest_framework.serializers import ModelSerializer
-from .models import GGITUser, NoteElement
+from .models import GGITUser, NoteElement, Note
 from django.shortcuts import (get_object_or_404, render, HttpResponseRedirect)
 
 
@@ -8,8 +9,37 @@ class GGITUserSerializer(ModelSerializer):
         model = GGITUser
         exclude = ['password', 'is_superuser', 'last_login']
 
-class Note_element(ModelSerializer):
+class Note_elementSerializer(ModelSerializer):
     class Meta:
         model = NoteElement
         exclude = ['note']
+
+
+class NoteSerializer(ModelSerializer):
+    note_elements = Note_elementSerializer(many=True)
+    
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+    def create(self, data):
+        note_elements = data.pop('note_elements')
+        note = Note.objects.create(data)
+
+        for item in note_elements:
+            NoteElement.objects.create(note = note, tag=item['tag'], content = item['content'])
+        
+        return note
+
+    def update(self,instance: Note, data):
+        instance.note_elements.all().delete()
+        note_elements = data.pop('note_elements')
+
+        for item in note_elements:
+            NoteElement.objects.create(note = instance, tag = item['tag'], content = item['content'])
+
+        return instance
+
+
+
 
